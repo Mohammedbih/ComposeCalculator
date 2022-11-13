58a68a3d9673cdf95ed3d3dc13a704037ed3624c
Moody <asdas201888@gmail.com>
2022-11-13 13:54:52 +0200

    Initial commit

---
+++ app/src/main/java/com/example/composecalculator/CalculatorButton.kt
@@ -0,0 +1,57 @@
+package com.example.composecalculator
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CalculatorButton(
+    symbol: String,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .clip(MaterialTheme.shapes.extraLarge)
+            .clickable { onClick() }
+            .then(modifier)
+    ) {
+        Text(
+            text = symbol,
+            style = MaterialTheme.typography.displayLarge,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun PrevButton() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+
+        CalculatorButton(
+            symbol = "+",
+            modifier = Modifier
+                .background(Color.Cyan)
+                .width(150.dp)
+        ) {
+
+        }
+    }
+}
+
